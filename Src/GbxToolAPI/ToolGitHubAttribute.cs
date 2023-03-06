5a4f3a14bf008cd8dd5918283157dc4686c00d80
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-06 16:38:52 +0100

    Add ToolGitHubAttribute

---
+++ Src/GbxToolAPI/ToolGitHubAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace GbxToolAPI;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ToolGitHubAttribute : Attribute
+{
+    public string Repository { get; }
+    
+    public ToolGitHubAttribute(string repository)
+    {
+        Repository = repository;
+    }
+}
