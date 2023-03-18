b58d449444260d9b53eca40ff4299677d34f6829
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-18 03:28:02 +0100

    Add GameVersion

---
+++ Src/GbxToolAPI/GameVersion.cs
@@ -0,0 +1,16 @@
+﻿using GBX.NET.Engines.Game;
+
+namespace GbxToolAPI;
+
+public static class GameVersion
+{
+    public static bool IsManiaPlanet(CGameCtnReplayRecord replay)
+    {
+        return replay.Chunks.Any(chunk => chunk.Id > 0x03093015 && chunk.Id <= 0x03093FFF);
+    }
+    
+    public static bool IsManiaPlanet(CGameCtnGhost ghost)
+    {
+        return ghost.Chunks.Any(chunk => chunk.Id > 0x03092019 && chunk.Id <= 0x03092FFF);
+    }
+}
