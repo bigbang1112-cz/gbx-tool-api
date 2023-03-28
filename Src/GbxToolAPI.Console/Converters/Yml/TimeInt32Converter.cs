00fb80b32a5ea834fba46911ee2348ad9d9ee7c0
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-29 01:49:56 +0200

    Add Yml wrap

---
+++ Src/GbxToolAPI.Console/Converters/Yml/TimeInt32Converter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using TmEssentials;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace GbxToolAPI.Console.Converters.Yml;
+
+public sealed class TimeInt32Converter : IYamlTypeConverter
+{
+    public bool Accepts(Type type) => type == typeof(TimeInt32);
+
+    public object? ReadYaml(IParser parser, Type type)
+    {
+        return TimeInt32.FromMilliseconds(int.Parse(parser.Consume<Scalar>().Value));
+    }
+
+    public void WriteYaml(IEmitter emitter, object? value, Type type)
+    {
+        var val = (TimeInt32)value!;
+
+        emitter.Emit(new Scalar(val.TotalMilliseconds.ToString()));
+    }
+}
