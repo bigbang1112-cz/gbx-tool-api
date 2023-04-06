928e8e42540590861165a054649e1853de7120b9
Petr Pivoňka <petrpiv1@gmail.com>
2023-04-06 03:07:49 +0200

    Add IsManiaPlanet to NodeFile

--- Src/GbxToolAPI/NodeFile.cs
+++ Src/GbxToolAPI/NodeFile.cs
@@ -2,7 +2,7 @@
 
 namespace GbxToolAPI;
 
-public record NodeFile<T>(T Node, string? FileName = null) where T : Node
+public record NodeFile<T>(T Node, string? FileName = null, bool IsManiaPlanet = false) where T : Node
 {
     public static explicit operator T(NodeFile<T> nodeFile)
     {
