b9bdb4c6a81e7f57b3eb15c3b13f6604595410bd
Petr Pivoňka <petrpiv1@gmail.com>
2023-04-13 21:14:15 +0200

    Add common properties

--- Src/GbxToolAPI.Client/ToolComponentBase.cs
+++ Src/GbxToolAPI.Client/ToolComponentBase.cs
@@ -4,4 +4,31 @@ namespace GbxToolAPI.Client;
 
 public abstract class ToolComponentBase<T> : ComponentBase where T : class, ITool
 {
+    private string selectedConfigName = "Default";
+
+    [Parameter]
+    [EditorRequired]
+    public required string Route { get; set; }
+
+    public string SelectedConfigName
+    {
+        get => selectedConfigName;
+        set
+        {
+            selectedConfigName = value;
+            SyncLocalStorage.SetItem($"Tool:{Route}:Config:Selected", selectedConfigName);
+        }
+    }
+
+    [Parameter]
+    [EditorRequired]
+    public required Dictionary<string, Config> Configs { get; set; } = new();
+
+    [Inject]
+    public required Blazored.LocalStorage.ISyncLocalStorageService SyncLocalStorage { get; set; }
+
+    public ToolComponentBase()
+    {
+        
+    }
 }
