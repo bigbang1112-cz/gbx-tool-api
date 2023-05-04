4defb88ebc1e1d307087845c31a270c516ddbe01
Petr Pivoňka <petrpiv1@gmail.com>
2023-05-04 05:16:19 +0200

    Add Constants to Server

---
+++ Src/GbxToolAPI.Server/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace GbxToolAPI.Server;
+
+public static class Constants
+{
+    public const string InsiderPolicy = "InsiderPolicy";
+    public const string SuperAdminPolicy = "SuperAdminPolicy";
+    public const string SuperAdmin = "SuperAdmin";
+}
