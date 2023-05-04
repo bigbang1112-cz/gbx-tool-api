4bad8f0ab97160f3927c8b8207cce1e6ca9c50ad
Petr Pivoňka <petrpiv1@gmail.com>
2023-05-04 05:22:24 +0200

    Update deps

--- Src/GbxToolAPI.Server/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GbxToolAPI.Server;
-
-public static class Constants
-{
-    public const string InsiderPolicy = "InsiderPolicy";
-    public const string SuperAdminPolicy = "SuperAdminPolicy";
-    public const string SuperAdmin = "SuperAdmin";
-}
