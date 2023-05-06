69e87143700fef6c03130a1b0a2a6ad844a564a2
Petr Pivoňka <petrpiv1@gmail.com>
2023-05-06 21:04:51 +0200

    Add GetExtendedAsciiValid

--- Src/GbxToolAPI/RegexUtils.cs
+++ Src/GbxToolAPI/RegexUtils.cs
@@ -6,9 +6,18 @@ public partial class RegexUtils
 {
     [GeneratedRegex("(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z0-9])")]
     private static partial Regex RegexPascalCaseToKebabCase();
+    
+    [GeneratedRegex(@"[^\u0000-\u00FF]")]
+    private static partial Regex RegexExtendedAsciiValid();
 
     public static string PascalCaseToKebabCase(string str)
     {
         return RegexPascalCaseToKebabCase().Replace(str, "-$1").Trim().ToLower();
     }
+
+    public static string GetExtendedAsciiValid(string str)
+    {
+        return RegexExtendedAsciiValid().Replace(str, "_");
+    }
+
 }
